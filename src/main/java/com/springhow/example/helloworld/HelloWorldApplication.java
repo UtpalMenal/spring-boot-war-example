90d1096801818ecd843cd46f146f5af2bba18687
UtpalMenal <92942188+UtpalMenal@users.noreply.github.com>
2024-12-29 18:35:20 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "This Utpal maven build pipeline";
+        return "This Utpal maven build pipeline with  webhook";
     }
 
 }
